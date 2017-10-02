0a60b7f262defa89c3cff00f71866e0995318c1d
Taras_Fedorov <CkfdfDtktce06~>
2017-10-02 14:07:28 +0300

    sdist simple result

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Tags_calculator',
+    version='0.1dev',
+    packages=['package', 'package\\tags_extractor'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    author="Taras Fedorov",
+    author_email="t.fedorov.s@gmail.com"
+)
